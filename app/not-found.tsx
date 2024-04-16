0640ff4a3abf0c2432e6e914ec5837a13c09a9ca
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-16 13:21:56 -0500

    Finish Cloudflare setup

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export const runtime = 'edge';
+
+export default function Home() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        not found
+      </main>
+    );
+  }
+  
