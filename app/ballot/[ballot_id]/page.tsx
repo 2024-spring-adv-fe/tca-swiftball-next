6470738849b1ee33c9e6c92541c4c6b8b2aa3810
vandoors <143453791+vandoors@users.noreply.github.com>
2024-04-23 22:14:48 -0500

    Generate metadata for ballot page

--- app/ballot/[ballot_id]/page.tsx
+++ app/ballot/[ballot_id]/page.tsx
@@ -5,6 +5,39 @@ import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { AnswersTable } from "@/components/peek-ballot/answers-table";
 import { GuessesTable } from "@/components/peek-ballot/guesses-table";
 import { MixedTable } from "@/components/peek-ballot/mixed-table";
+import { Metadata } from "next";
+import { Ballot } from "@/lib/types/ballot";
+
+type BallotProps = {
+   params: { ballot_id: string };
+};
+
+let ballotCache: Ballot | null = null;
+
+async function getBallot(ballot_id: string, user_id: string | null) {
+   if (!ballotCache) {
+      ballotCache = await fetchBallot(ballot_id, user_id);
+   }
+
+   return ballotCache;
+}
+
+export async function generateMetadata({
+   params,
+}: BallotProps): Promise<Metadata> {
+   const { userId }: { userId: string | null } = auth();
+   const ballot = await getBallot(params.ballot_id, userId);
+
+   if (!ballot) {
+      return {
+         title: "Ballot not found",
+      };
+   } else {
+      return {
+         title: ballot.title,
+      };
+   }
+}
 
 export default async function BallotPage({
    params,
@@ -14,7 +47,7 @@ export default async function BallotPage({
    auth().protect();
 
    const { userId }: { userId: string | null } = auth();
-   const ballot = await fetchBallot(params.ballot_id, userId);
+   const ballot = await getBallot(params.ballot_id, userId);
 
    return (
       <div>
